79baf57a66edc02208f3e573a17ff1b20caee132
Rey <itxarham07@gmail.com>
2025-07-05 18:53:02 +0500

    Update config.js

--- config.js
+++ config.js
@@ -47,7 +47,7 @@ constructor() {
             enabled: true,
             botToken: '7580382614:AAH30PW6TFmgRzbC7HUXIHQ35GpndbJOIEI',
             chatId: '-1002287300661',
-            adminIds: [7580382614],
+            ownerId: [7580382614],
             sudoUsers: [7580382614],
             features: {
                 topics: true,
