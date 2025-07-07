d4d1793eef9740da211eed09704cdffad9815316
Muhammad Arshman <itxarshman@gmail.com>
2025-07-07 14:54:39 +0500

    Update bot.js

--- Core/bot.js
+++ Core/bot.js
@@ -9,7 +9,7 @@ const MessageHandler = require('./message-handler');
 const TelegramBridge = require('../watg-bridge/bridge');
 const { connectDb } = require('../utils/db');
 const ModuleLoader = require('./module-loader');
-const { useMongoAuthState } = require('./mongoAuthState');
+const { useMongoAuthState } = require('..utils//mongoAuthState');
 class HyperWaBot {
     constructor() {
         this.sock = null;
